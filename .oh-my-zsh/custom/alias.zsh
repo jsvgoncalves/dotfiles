9d7bfb16516580f894db16cf0bd250cc7291e37c
João Gonçalves <jsvgoncalves@gmail.com>
2014-08-26 21:18:20 +0200

    oh-my-zsh: Added aliases. * Some utility aliases.
    Signed-off-by: Joao Goncalves <jsvgoncalves@gmail.com>

---
+++ .oh-my-zsh/custom/alias.zsh
@@ -0,0 +1,20 @@
+# LS_COLORS=true
+# export LS_COLORS
+
+# Tree levels and colorized
+alias t='tree -C -L 1'
+alias t1='tree -C -L 1'
+alias t2='tree -C -L 2'
+alias t3='tree -C -L 3'
+alias t4='tree -C -L 4'
+
+# Rails stuff
+alias rs='rails server'
+alias rdb='rake db:migrate'
+alias rdbc='rake db:drop:all'
+
+# Nautilus
+alias here='nautilus .'
+
+# apt-get
+alias i='sudo apt-get install'
